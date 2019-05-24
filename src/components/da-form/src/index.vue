d17669223fa0aac90191fc3c100de5e42c0cfccd
rockey <rockey543400@gmail.com>
2019-05-24 11:24:51 +0800

    update

--- src/components/da-form/src/index.vue
+++ src/components/da-form/src/index.vue
@@ -9,18 +9,22 @@
                     <span>{{item.label}}</span>
                 </label>
                 <div class="flex flex-inline input-box">
-                    <input v-model="item.value" type="text" :readonly="item.readonly" :autofocus="item.autofocus"
-                           :placeholder="item.placeholder"
+                    <input v-model="item.value" type="text" @focus="focus(index)" @blur="blur(index)"
+                           :readonly="item.readonly"
+                           :autofocus="item.autofocus"
+                           :placeholder="focusLineIndex===index?'':item.placeholder"
                            @click.stop="item.on.click({position:'input-box',source:item})">
 
                     <!--delete位置插槽-->
-                    <slot :name="item.positionSlots.delete.name"
-                          v-if="item.positionSlots&&item.positionSlots.delete"></slot>
-                    <slot name="delete" v-else>
-                        <da-icon class="da-icon delete" :name="icons.delete"
-                                 @click="isDelete(item)">
-                        </da-icon>
-                    </slot>
+                    <div class="flex flex-inline slot-delete">
+                        <slot :name="item.positionSlots.delete.name"
+                              v-if="item.positionSlots&&item.positionSlots.delete"></slot>
+                        <slot name="delete" v-else>
+                            <da-icon class="da-icon delete" :name="icons.delete"
+                                     @click="isDelete(item)">
+                            </da-icon>
+                        </slot>
+                    </div>
 
                 </div>
             </div>
@@ -31,6 +35,7 @@
 
 <script>
     import "@/assets/style/common/index.less"
+    import "@/assets/style/animations/index.less"
     import DaIcon from "../../da-icon/src";
     import utils from "@/utils"
 
@@ -66,6 +71,7 @@
         },
         data() {
             return {
+                focusLineIndex: null,
                 initForm: [],
                 icons: {
                     delete: "feather-x"
@@ -74,6 +80,12 @@
             }
         },
         methods: {
+            async blur(index = null) {
+                this.focusLineIndex = null;
+            },
+            async focus(index = null) {
+                this.focusLineIndex = index;
+            },
             async isDelete(item) {
                 item.value = "";
                 item.on.click({position: 'delete', source: item});
@@ -237,12 +249,34 @@
 </script>
 
 <style lang="less" scoped>
+
+    @border-bottom-color: #e7e7e7;
+    @label-color: #666666;
+    @placeholder-color: #999999;
+    @da-icon-color: @label-color;
+
     .da-form {
         form {
             .line {
-                height: 0.6rem;
+                height: 0.56rem;
                 padding: 0 0.12rem;
                 background-color: white;
+                position: relative;
+
+                &:last-child {
+                    &:after {
+                        display: none;
+                    }
+                }
+
+                &:after {
+                    content: "";
+                    width: 93.5%;
+                    height: 1px;
+                    background-color: @border-bottom-color;
+                    position: absolute;
+                    bottom: 0;
+                }
 
                 label, .input-box {
                     height: 100%;
@@ -250,23 +284,40 @@
 
                 label {
                     flex: 0.7;
+                    color: @label-color;
                 }
 
                 .input-box {
-                    flex: auto;
+                    flex: 2.6;
                     position: relative;
 
                     input {
                         flex: auto;
                         height: 100%;
+
+                        &::placeholder {
+                            color: @placeholder-color;
+                        }
                     }
 
-                    .da-icon {
-                        flex: 0.23;
-                        font-size: 0.23rem;
+                    .slot-delete {
+                        flex: 0.25;
                         justify-content: flex-end;
-                        right: -0.05rem;
                         height: 100%;
+
+                        .da-icon {
+                            font-size: 0.23rem;
+                            color: @da-icon-color;
+                            transition: transform 0.3s;
+
+                            &:active {
+                                transform: scale(1.5);
+                            }
+
+                            &.delete {
+                                right: -0.05rem;
+                            }
+                        }
                     }
                 }
             }
