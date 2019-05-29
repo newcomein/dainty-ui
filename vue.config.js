bc14bf2f1befc192fe6b31314fee20a40bba9251
rockey <rockey543400@gmail.com>
2019-05-29 20:34:06 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,22 @@ const MinifyPlugin = require("babel-minify-webpack-plugin");
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 module.exports = {
     productionSourceMap: false,
-    // configureWebpack: {
-    //     optimization: {
-    //         minimizer: [new UglifyJsPlugin({
-    //             cache: true,
-    //             parallel: true
-    //         })]
-    //     },
-    //     plugins: [new MinifyPlugin()],
-    // }
-};
+    configureWebpack: (config) => {
+        let _config = {};
+        if (process.env.NODE_ENV === "production") {
+
+            _config = {
+                optimization: {
+                    minimizer: [new UglifyJsPlugin({
+                        cache: true,
+                        parallel: true
+                    })]
+                },
+                plugins: [new MinifyPlugin()],
+            }
+
+        }
+        return _config
+    }
+}
+;
