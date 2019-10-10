4db50e22aa789c2415a2e77a2f0a5f6b226d56be
rockey <rockey543400@gmail.com>
2019-10-10 20:39:17 +0800

    更新

--- src/components/da-dialog/src/index.vue
+++ src/components/da-dialog/src/index.vue
@@ -7,7 +7,7 @@
         <transition mode="in-out" enter-active-class="animated fadeIn"
                     leave-active-class="animated faster fadeOut">
             <div v-if="isShow" class="da-flex da-flex-center da-dialog-box" @touchmove.prevent="" @mousewheel.prevent=""
-                 @click="$emit('update:isShow',!isShow)">
+                 @click="closeOnClickModalHandle">
                 <div class="da-flex da-flex-center da-outsideBox">
                     <div class="da-flex da-flex-center da-flex-column da-block">
                         <div class="da-flex da-content" @click.stop="" @touchmove.stop="" @mousewheel.stop="">
@@ -34,6 +34,18 @@
             isShow: {
                 type: Boolean,
                 default: false,
+            },
+            //是否可以通过点击 modal 关闭 Dialog
+            closeOnClickModal: {
+                type: Boolean,
+                default: true
+            }
+        },
+        methods: {
+            async closeOnClickModalHandle() {
+                if (this.closeOnClickModal) {
+                    this.$emit('update:isShow', !this.isShow)
+                }
             }
         }
     }
